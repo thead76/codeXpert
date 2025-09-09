1a0d9363fb29f46e44113e116e84c97518ce842d
Aditya Kr <gadityakumar77@gmail.com>
2025-09-10 00:55:19 +0530

    Landing page

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="/codeXpert" >
     <App />
   </BrowserRouter>
 );
