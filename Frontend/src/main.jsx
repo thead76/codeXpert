2574baef0ab8c87bee7b2c28a7e7fa2d6fd09701
Aditya Kr <gadityakumar77@gmail.com>
2025-09-10 00:21:41 +0530

    Deploy

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,40 +1,12 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-// import App from "./App";
-// import "./index.css"; // Tailwind styles
-// import "./fonts.css"; // fonts
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <BrowserRouter basename="/" >
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
-// import ReactDOM from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-// import App from "./App";
-// import "./index.css"; // Tailwind CSS
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <BrowserRouter basename="/">
-//     <App />
-//   </BrowserRouter>
-// );
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter basename="/">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
-
