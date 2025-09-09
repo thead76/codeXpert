a51b6f274a7d4a8871fe256d0d8bf721c07f21ea
Aditya Kr <gadityakumar77@gmail.com>
2025-09-10 00:08:45 +0530

    Deploy

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -7,7 +7,7 @@ import "./fonts.css"; // fonts
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/" >
       <App />
     </BrowserRouter>
   </React.StrictMode>
