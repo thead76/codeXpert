8a49be9c33fe2088a87d0783206aa33459966be2
Aditya Kr <gadityakumar77@gmail.com>
2025-10-10 14:58:07 +0530

    Added Nodemailer

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -4,7 +4,9 @@ import jwt from 'jsonwebtoken';
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
