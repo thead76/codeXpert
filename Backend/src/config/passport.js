ab4c8d2f603aad5c8dc35d9d616233292a3ef9f4
Aditya Kr <gadityakumar77@gmail.com>
2025-10-10 14:09:09 +0530

    Callback URL Added

--- Backend/src/config/passport.js
+++ Backend/src/config/passport.js
@@ -7,7 +7,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/api/v1/auth/google/callback',
+      // यह लाइन बदली गई है
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
